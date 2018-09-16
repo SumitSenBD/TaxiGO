06ba696fb394c32bbebf8bd033274031e571ca18
Mate Bence <m.bence05@gmail.com>
2018-09-16 17:53:33 +0200

    cast 'Pridaj sa' hotova

--- _js/taxi_go_main.js
+++ _js/taxi_go_main.js
@@ -1,3 +1,33 @@
-(function($) {
-  "use strict";
-})(jQuery);
+$(document).ready(function(){
+    $("a.js-scroll-trigger").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+
+    $('.js-scroll-trigger').click(function() {
+        $('.navbar-collapse').collapse('hide');
+    });
+
+    $('body').scrollspy({
+        target: '#navigation',
+        offset: 56
+    });
+
+    var navbarCollapse = function() {
+        if ($("#navigation").offset().top > 100) {
+            $("#navigation").addClass("navbar-shrink");
+        } else {
+            $("#navigation").removeClass("navbar-shrink");
+        }
+    };
+
+    navbarCollapse();
+    $(window).scroll(navbarCollapse);
+});
