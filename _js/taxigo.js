bb4f6ce5400557e275858f00f4486625e61d451d
Mate Bence <m.bence05@gmail.com>
2018-09-17 19:29:57 +0200

    Pridanie animacii, a kniznice wow.js animate.css

--- _js/taxigo.js
+++ _js/taxigo.js
@@ -1,17 +1,19 @@
-$(document).ready(function () {
-    $("a.js-scroll-trigger").on('click', function (event) {
+$(document).ready(function(){
+    new WOW().init();
+
+    $("a.js-scroll-trigger").on('click', function(event) {
         if (this.hash !== "") {
             event.preventDefault();
             var hash = this.hash;
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 800, function () {
+            }, 800, function(){
                 window.location.hash = hash;
             });
         }
     });
 
-    $('.js-scroll-trigger').click(function () {
+    $('.js-scroll-trigger').click(function() {
         $('.navbar-collapse').collapse('hide');
     });
 
@@ -20,7 +22,7 @@ $(document).ready(function () {
         offset: 56
     });
 
-    var navbarCollapse = function () {
+    var navbarCollapse = function() {
         if ($("#navigation").offset().top > 100) {
             $("#navigation").addClass("navbar-shrink");
         } else {
